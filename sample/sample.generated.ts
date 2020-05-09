78b8a5ca54c5b3a2ee28af50126c4c7b01436d51
whatasoda <git@whatasoda.me>
2020-05-09 23:10:11 +0900

    chore: add a sample

---
+++ sample/sample.generated.ts
@@ -0,0 +1,50 @@
+// DO NOT EDIT - GENERATED FILE
+// typeshot-output-header
+// typeshot-output-header
+import type { Options } from 'prettier';
+interface SomeInterface {}
+type SomeType = {};
+export type Hoge = {
+  hoge: {
+    name: 'hoge';
+    value: SomeType;
+  };
+  fuga: {
+    name: 'fuga';
+    value: SomeInterface;
+  };
+  prettier: {
+    name: 'prettier';
+    value: Options;
+  };
+};
+
+// hogehoge
+export type Sample = {
+  hoge: {
+    name: 'hoge';
+    value: SomeType;
+  };
+};
+
+// fuga
+export type Sample0 = {
+  fuga: {
+    name: 'fuga';
+    value: SomeInterface;
+  };
+};
+
+export type HogeSelf = {
+  name: 'hoge';
+  value: SomeType;
+};
+
+export type FugaSelf = {
+  name: 'fuga';
+  value: SomeInterface;
+};
+// typeshot-output-footer
+// eslint-disable-next-line no-console
+console.log();
+export {};
---
+++ sample/sample.typeshot.ts
@@ -0,0 +1,63 @@
+// typeshot-output-header
+import type { Options } from 'prettier';
+interface SomeInterface {}
+type SomeType = {};
+
+// typeshot-header
+import typeshot from '../src/typeshot';
+
+interface SomeTypeMap {
+  hoge: SomeType;
+  fuga: SomeInterface;
+  prettier: Options;
+}
+type MappedType<T extends Record<string, any>> = {
+  [K in keyof T]: { name: K; value: T[K] };
+};
+// typeshot-main
+
+/** define output destination and header */
+typeshot.configuration({ output: './sample.generated.ts' })`
+// DO NOT EDIT - GENERATED FILE
+`;
+
+typeshot.takeStatic<MappedType<SomeTypeMap> /* target type */>('Hoge' /* internal key */, 'Hoge' /* name */)`
+  export ${typeshot.TemplateSymbols.DECLARATION}
+`;
+
+interface DynamicSampleProps {
+  name: string;
+  description: string;
+  pick: string;
+}
+
+const dynamicSample = typeshot
+  .createDynamic<Pick<MappedType<SomeTypeMap>, typeshot.T /* this will be replaced */>>('sample' /* internal key */)
+  .parameters<DynamicSampleProps>(({ pick }) => [pick /* type parameter */])
+  .names(({ name }) => name)`
+  // ${({ description }) => description}
+  export ${typeshot.TemplateSymbols.DECLARATION}
+`;
+
+dynamicSample({ name: 'Sample', description: 'hogehoge', pick: 'hoge' });
+dynamicSample({ name: 'Sample0', description: 'fuga', pick: 'fuga' });
+
+interface DynamicSample2Props {
+  name: typeshot.NameDescriptor;
+}
+
+const dynamicSample2 = typeshot
+  .createDynamic<MappedType<SomeTypeMap>>('sample2' /* internal key */)
+  .parameters<DynamicSample2Props>(() => [])
+  .names(({ name }) => name)`
+  export ${typeshot.TemplateSymbols.DECLARATION}
+`;
+/* generate types of each properties */
+dynamicSample2({ name: { hoge: 'HogeSelf', fuga: 'FugaSelf' } });
+
+// typeshot-footer
+// eslint-disable-next-line no-console
+console.log();
+// typeshot-output-footer
+// eslint-disable-next-line no-console
+console.log();
