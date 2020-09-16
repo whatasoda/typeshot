05f7974924d749c1292878c365e51e9015080619
whatasoda <git@whatasoda.me>
2020-09-16 15:56:29 +0900

    chore: remove old implementation

--- src/program/ast-utils.ts
+++ src/program/ast-utils.ts
@@ -1,39 +1,5 @@
 import ts from 'typescript';
 
-type CallLikeExpression = Exclude<
-  ts.CallLikeExpression,
-  ts.Decorator | ts.JsxOpeningElement | ts.JsxSelfClosingElement
->;
-
-/**
- * Note that ts.Decorator, ts.JsxOpeningElement and ts.JsxSelfClosingElement are not included.
- */
-export const flattenCallLikeExpressionChain = (entry: ts.Expression) => {
-  const result: CallLikeExpression[] = [];
-
-  let curr: ts.Expression | null = entry;
-  while (curr) {
-    if (ts.isTaggedTemplateExpression(curr)) {
-      result.push(curr);
-      curr = curr.tag;
-      continue;
-    }
-    if (ts.isCallExpression(curr) || ts.isNewExpression(curr)) {
-      result.push(curr);
-      curr = curr.expression;
-      continue;
-    }
-
-    if (ts.isPropertyAccessExpression(curr) || ts.isElementAccessExpression(curr)) {
-      curr = curr.expression;
-      continue;
-    }
-    curr = null;
-  }
-
-  return result.reverse();
-};
-
 export const forEachChildDeep = (root: ts.Node, callback: (node: ts.Node) => void) => {
   const queue: ts.Node[] = [root];
   while (queue.length) {
