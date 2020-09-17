5748a84e1d31147b3febfb3d827d932df8111aaa
whatasoda <git@whatasoda.me>
2020-09-17 12:25:12 +0900

    feat: add run-single-in-subprocess

---
+++ src/program/run-single-in-subprocess.ts
@@ -0,0 +1,38 @@
+import type ts from 'typescript';
+import { exec } from 'child_process';
+import { runSingle, TypeshotOptions } from './run-single';
+
+const runtime = /\.tsx?$/.test(__filename) ? 'ts-node-transpile-only' : 'node';
+
+export const runSingleInSubprocess = (options: TypeshotOptions, systemModulePath: string = 'typescript') => {
+  return new Promise((resolve, reject) => {
+    const cp = exec(`${runtime} ${__filename} '${JSON.stringify(options)}' ${systemModulePath}`);
+    cp.stdout?.pipe(process.stdout);
+    cp.stderr?.pipe(process.stderr);
+    cp.on('close', (code) => (code === 0 ? resolve() : reject()));
+  });
+};
+
+if (require.main === module) {
+  const getSystem = (systemModulePath: string) => {
+    const systemModule = require(systemModulePath);
+    if (!systemModule || !('sys' in systemModule)) {
+      throw new Error('');
+    }
+    return systemModule.sys as ts.System;
+  };
+
+  const main = async () => {
+    const [, , optionsJson, systemSourcePath] = process.argv;
+    const options = JSON.parse(optionsJson) as TypeshotOptions;
+    const sys = getSystem(systemSourcePath);
+    try {
+      await runSingle(sys, options);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e);
+    }
+  };
+
+  main();
+}
